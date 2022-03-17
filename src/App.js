6925b7aee6d90392a2b89f2a42beb870ff67ef6b
vickyruud <aprajit.k@gmail.com>
2022-03-17 16:52:01 -0400

    change timer to 24 hours countdown

--- src/App.js
+++ src/App.js
@@ -8,28 +8,24 @@ function App() {
     hours:10,
     minutes:10,
     seconds: 10,
+    eatingWindow:false,
     
   })
 
-  let interval;
 
 
 
   const startTimer = () => {
-    const countDownDate = new Date("March 18, 2022 15:00:00 ").getTime();
 
-    interval = setInterval(() => {
-      const now = new Date().getTime();
+    const interval = setInterval(() => {
+      const today = new Date()
+      if (today.getHours() >= 10 && today.getHours() <= 18) {
+      }
+      const hours = 24 - today.getHours()
+      const minutes = 60 - today.getHours();
+      const seconds = 60 - today.getSeconds();
 
-      const distance = countDownDate - now;
-      const hours = Math.floor(distance % (24 * 60 * 60 * 1000) / (1000 * 60 * 60));
-      const minutes = Math.floor(distance % (60 * 60 * 1000) / (1000 * 60));
-      const seconds = Math.floor(distance % (60 * 1000) / 1000);
-
-      if (distance < 0) {
-        //stop timer
-        clearInterval(interval.current);
-      } else {
+     
         //update timer 
         setState({
           ...state,
@@ -37,9 +33,6 @@ function App() {
           minutes: minutes,
           seconds: seconds
         })
-
-      }
-
       
     })
 
