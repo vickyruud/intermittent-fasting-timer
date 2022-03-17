1638e7d49f57ce32162b82926c1bf1280d79fcab
vickyruud <aprajit.k@gmail.com>
2022-03-17 16:01:35 -0400

    add logic for countdown

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Clock from './components/Clock';
 
@@ -11,7 +11,43 @@ function App() {
     seconds:10
   })
 
+  let interval;
 
+  const startTimer = () => {
+    const countDownDate = new Date("May 4, 2022").getTime();
+
+    interval = setInterval(() => {
+      const now = new Date().getTime();
+
+      const distance = countDownDate - now;
+      const days = Math.floor(distance / (24 * 60 * 60 * 1000));
+      const hours = Math.floor(distance % (24 * 60 * 60 * 1000) / (1000 * 60 * 60));
+      const minutes = Math.floor(distance % (60 * 60 * 1000) / (1000 * 60));
+      const seconds = Math.floor(distance % (60 * 1000) / 1000);
+
+      if (distance < 0) {
+        //stop timer
+        clearInterval(interval.current);
+      } else {
+        //update timer 
+        setState({
+          ...state,
+          days: days,
+          hours: hours,
+          minutes: minutes,
+          seconds: seconds
+        })
+
+      }
+
+      
+    })
+
+  }
+
+  useEffect(() => {
+    startTimer();
+  })
 
   return (
     <div className="App">
