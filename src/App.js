f7baae87d39812e43fcdd1ffc3219d6ac58cc6ff
vickyruud <aprajit.k@gmail.com>
2022-03-17 16:31:31 -0400

    removed days

--- src/App.js
+++ src/App.js
@@ -5,22 +5,23 @@ import Clock from './components/Clock';
 function App() {
   
   const [state, setState] = useState({
-    days: 10,
     hours:10,
     minutes:10,
-    seconds:10
+    seconds: 10,
+    
   })
 
   let interval;
 
+
+
   const startTimer = () => {
-    const countDownDate = new Date("May 4, 2022").getTime();
+    const countDownDate = new Date("March 18, 2022 15:00:00 ").getTime();
 
     interval = setInterval(() => {
       const now = new Date().getTime();
 
       const distance = countDownDate - now;
-      const days = Math.floor(distance / (24 * 60 * 60 * 1000));
       const hours = Math.floor(distance % (24 * 60 * 60 * 1000) / (1000 * 60 * 60));
       const minutes = Math.floor(distance % (60 * 60 * 1000) / (1000 * 60));
       const seconds = Math.floor(distance % (60 * 1000) / 1000);
@@ -32,7 +33,6 @@ function App() {
         //update timer 
         setState({
           ...state,
-          days: days,
           hours: hours,
           minutes: minutes,
           seconds: seconds
--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -9,12 +9,7 @@ const Clock = (props) => {
       <section className="timer-container">
         <section className="timer">
           <div className="clock">
-            <section>
-              <p>{props.timer.days}</p>
-              <small>Days</small>
-            </section>
-            <span>:</span>
-            <section>
+           <section>
                <p>{props.timer.hours}</p>
               <small>Hours</small>{" "}
             </section>
